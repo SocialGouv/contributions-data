a363a920c7378165605415bdab3978b68fc0a928
Lionel <lionel@lumographe.fr>
2020-07-01 15:56:42 +0200

    feat(data): update data format (#15)
    * feat(data): update data format
    * fix(fetch): throw if idcc not found
    
    - add new dila* key in references
    - add internal id for question and answer
    
    BREAKING CHANGE: agreement is removed from references item

--- src/fetchContributions.js
+++ src/fetchContributions.js
@@ -1,7 +1,7 @@
-const fetch = require("node-fetch");
-const kaliData = require("@socialgouv/kali-data/data/index.json");
-const remark = require("remark");
-const strip = require("strip-markdown");
+import kaliData from "@socialgouv/kali-data/data/index.json";
+import fetch from "node-fetch";
+import remark from "remark";
+import strip from "strip-markdown";
 
 const mdStriper = remark().use(strip);
 
@@ -9,9 +9,57 @@ const API_URL =
   process.env.API_URL ||
   `https://contributions-api.codedutravail.fabrique.social.gouv.fr`;
 
-const comparableIdcc = (num) => parseInt(num);
+/** @type {Contributions.sortByFn} */
+const sortBy = (key) => (a, b) => `${a[key]}`.localeCompare(`${b[key]}`);
 
-const sortByKey = (key) => (a, b) => `${a[key]}`.localeCompare(`${b[key]}`);
+/**
+ *
+ * @param {string|number} num
+ */
+const comparableIdcc = (num) => parseInt(num.toString(), 10);
+
+/**
+ *
+ * @param {Contributions.AnswerRaw[]} answers
+ */
+function getGenericAnswer(answers) {
+  const genericAnswer = answers.find((answer) => answer.agreement === null);
+  if (!genericAnswer) {
+    return;
+  }
+  const genericTextAnswer = mdStriper
+    .processSync(genericAnswer.markdown)
+    .contents.toString()
+    .replace(/(\s)\s+/, "$1")
+    .trim();
+
+  return {
+    id: genericAnswer.id,
+    text: genericTextAnswer,
+    description:
+      genericTextAnswer.slice(0, genericTextAnswer.indexOf(" ", 150)) + "…",
+    references: genericAnswer.references,
+    markdown: genericAnswer.markdown,
+  };
+}
+
+/** @type {Contributions.createRefFn} */
+function createGetRefUrl(idcc) {
+  const agreement = kaliData.find(
+    (convention) => comparableIdcc(convention.num) === comparableIdcc(idcc)
+  );
+  if (!agreement) {
+    throw new Error(`agreement ${idcc} not found `);
+  }
+  return function getRefUrl(reference) {
+    if (reference.dila_id) {
+      reference.url = `https://beta.legifrance.gouv.fr/conv_coll/id/${reference.dila_id}/?idConteneur=${agreement.id}`;
+    } else if (agreement.url) {
+      reference.url = agreement.url;
+    }
+    return reference;
+  };
+}
 
 /**
  *
@@ -20,104 +68,37 @@ const sortByKey = (key) => (a, b) => `${a[key]}`.localeCompare(`${b[key]}`);
  * resolve CCN references by IDCC from @socialgouv/kali-data
  *
  */
-
-const fetchContributions = async () => {
-  const agreements = await fetch(`${API_URL}/agreements`).then((r) => r.json());
-
-  const answers = await fetch(
-    `${API_URL}/public_answers?select=*&order=updated_at.desc`
-  ).then((r) => r.json());
-
+async function fetchContributions() {
+  /** @type {Contributions.QuestionRaw[]} */
   const questions = await fetch(
-    `${API_URL}/questions?select=*&order=id`
+    `${API_URL}/questions?select=id,value,index,answers:public_answers(id,markdown:value,references:answers_references(title:value,url,dila_id,dila_cid),agreement(name,idcc,parent_id))&order=index`
   ).then((r) => r.json());
 
-  const references = await fetch(`${API_URL}/answers_references`).then((r) =>
-    r.json()
-  );
-
-  const getAnswer = (answerId) => answers.find((q) => q.id === answerId);
-
-  const getAgreement = (agreementId) =>
-    agreements.find((a) => a.id === agreementId);
-
-  const getAgreementIdcc = (agreementId) => {
-    const agreement = getAgreement(agreementId);
-    return agreement && agreement.idcc;
-  };
-
-  // get answer CC details from kali-data
-  const getConventionKali = (answerId) => {
-    const answer = getAnswer(answerId);
-    const agreement = answer.agreement_id && getAgreement(answer.agreement_id);
-    const conventionKali =
-      agreement &&
-      kaliData.find(
-        (convention) =>
-          comparableIdcc(convention.num) === comparableIdcc(agreement.idcc)
-      );
-    return conventionKali;
-  };
-
-  const getReferences = (answerId) =>
-    references
-      .filter((r) => r.answer_id === answerId)
-      .map(({ category, value: title, url }) => {
-        if (category === "agreement") {
-          return {
-            category,
-            title,
-            agreement: getConventionKali(answerId),
-          };
-        }
-        return { title, url, category };
-      })
-      .sort(sortByKey("title"));
-
-  const getConventionsAnswers = (questionId) =>
-    answers
-      .filter((a) => a.question_id === questionId && a.agreement_id !== null)
-      .map((ccAnswer) => ({
-        markdown: ccAnswer.value,
-        idcc: getAgreementIdcc(ccAnswer.agreement_id),
-        references: getReferences(ccAnswer.id),
-      }))
-      .sort((a, b) => parseInt(a.idcc, 10) - parseInt(b.idcc, 10));
-
-  const getGenericAnswer = (questionId) => {
-    const genericAnswer = answers.find(
-      (a) => a.question_id === questionId && a.agreement_id === null
-    );
-    if (!genericAnswer) {
-      return;
+  return /**@type {Contributions.Question[]} */ (questions.flatMap(
+    ({ id, index, value: title, answers }) => {
+      const genericAnswer = getGenericAnswer(answers);
+      if (!genericAnswer) return [];
+      return {
+        id,
+        index,
+        title,
+        answers: {
+          generic: genericAnswer,
+          conventions: answers
+            .filter((answer) => answer.agreement !== null)
+            .map((answer) => ({
+              id: answer.id,
+              idcc: answer.agreement.idcc,
+              markdown: answer.markdown,
+              references: answer.references
+                .map(createGetRefUrl(answer.agreement.idcc))
+                .sort(sortBy("title")),
+            }))
+            .sort(sortBy("idcc")),
+        },
+      };
     }
-    const genericTextAnswer = mdStriper
-      .processSync(genericAnswer.value)
-      .contents.replace(/(\s)\s+/, "$1")
-      .trim();
-    return {
-      text: genericTextAnswer,
-      description:
-        genericTextAnswer.slice(0, genericTextAnswer.indexOf(" ", 150)) + "…",
-      references: getReferences(genericAnswer.id),
-      markdown: genericAnswer.value,
-    };
-  };
-
-  const allAnswers = questions
-    .map(({ id, index, value: title }) => ({
-      id,
-      index,
-      title,
-      answers: {
-        generic: getGenericAnswer(id),
-        conventions: getConventionsAnswers(id),
-      },
-    }))
-    .filter((q) => q.answers.generic)
-    .sort((a, b) => a.index - b.index);
-
-  return allAnswers;
-};
+  ));
+}
 
 module.exports = fetchContributions;
